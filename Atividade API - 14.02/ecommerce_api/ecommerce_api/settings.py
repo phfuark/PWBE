e9a0aaf2c76e6f7461a3919489cd930d79c6bcd1
phfuark <paulohsilva2007@gmail.com>
2025-02-19 15:11:31 -0300

    19.02.2025 - Adiconando Link da Apresentação (Espiral)

--- Atividade API - 14.02/ecommerce_api/ecommerce_api/settings.py
+++ Atividade API - 14.02/ecommerce_api/ecommerce_api/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
